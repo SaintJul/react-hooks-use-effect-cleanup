476853e5d5cf7329af2459d24714d7b898b45683
SaintJul <favor.julius@gmail.com>
2021-10-11 23:42:51 -0400

    commit tree deb3c93428902cb7ab36c20a721e3e37b12f1869

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    
+    const time = setInterval(() => {
       setTime(new Date());
-    }, 1000);
+    }, 1000);    
+
+    return function cleanup(){
+      clearInterval(time)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
